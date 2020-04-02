38ec5ded4bc2b3788070636447270df8718d2249
liaolibo-jay <liaolibo-jay@outlook.com>
2020-04-02 17:58:25 +0800

    2020.04.02 optimized

--- OlympicsMedals.py
+++ OlympicsMedals.py
@@ -1,11 +1,12 @@
 import os
 
 class medal:
-    nation_name = 'china'
-    nglodens = 0
-    nslivers = 0
-    nbronzes = 0
-    totScore = 0
+    nglodens =0 
+    nslivers =0 
+    nbronzes =0 
+    totScore =0 
+    def __init__(self,nation_name):
+        self.nation_name = nation_name
     
     def addmedals(self):
         nranks = int(input('请输入%s名次:' %(self.nation_name)))
@@ -15,34 +16,27 @@ class medal:
             self.nslivers += 1
         elif nranks == 3:
             self.nbronzes += 1
-        else:
-            pass
         self.totScore =self.nbronzes+self.nslivers+self.nglodens
 
     def outmedals(self):
         print('%s的奖牌数为:金牌%i块，银牌%i块，铜牌%i块。总数为%i.'%(self.nation_name,self.nglodens,self.nslivers,self.nbronzes,self.totScore))
 
 acountry = []
-NumCountry =0
 while True:
     name = input('请输入一个国家名：')
     if not name:
         break
     else:
-        acountry.append(medal())
-        acountry[NumCountry].nation_name = name
-    NumCountry += 1
+        acountry.append(medal(name))
 
 while True:
     inMedal = input('是否更新奖牌情况（输入y或者n）:')
-    if inMedal == 'n':
+    if inMedal != 'y':
         break
-    elif inMedal == 'y':
+    else:
         for i in acountry:
             i.addmedals()
         acountry = sorted(acountry,key=lambda medal:(medal.totScore,medal.nglodens,medal.nslivers,medal.nbronzes) ,reverse=True)
         
         for i in acountry:
             i.outmedals()
-    else:
-        break
